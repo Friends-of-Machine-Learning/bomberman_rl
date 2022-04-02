bb7ca3b879f8a5911d97b0d3c32a5d7e8686454d
Justin Sostmann <justin.sostmann@googlemail.com>
2022-04-02 15:15:51 +0200

    Add comments to the opti_agent.

--- agent_code/opti_agent/callbacks.py
+++ agent_code/opti_agent/callbacks.py
@@ -1,15 +1,21 @@
-from ..qtable_agent.callbacks import act as act_foml
-from ..qtable_agent.callbacks import setup as setup_foml
-from ..qtable_agent.callbacks import state_to_features as state_to_features_foml
+"""
+This agent is used as an alternative agent runner for other agents.
+It it used to check the performance of the agent each round and determine wether or not to update the saved model.
+This allows the agent to rollback to a save state any time it starts to loose performance.
+To use it for any agent simply change the imported agent to the agent you want to optimize.
+"""
+from ..qtable_agent.callbacks import act as act_other
+from ..qtable_agent.callbacks import setup as setup_other
+from ..qtable_agent.callbacks import state_to_features as state_to_features_other
 
 
 def setup(self):
-    setup_foml(self)
+    setup_other(self)
 
 
 def act(self, state):
-    return act_foml(self, state)
+    return act_other(self, state)
 
 
 def state_to_features(self, state):
-    return state_to_features_foml(self, state)
+    return state_to_features_other(self, state)
--- agent_code/opti_agent/train.py
+++ agent_code/opti_agent/train.py
@@ -1,29 +1,38 @@
 import pickle
 
-from ..qtable_agent.train import end_of_round as end_of_round_foml
-from ..qtable_agent.train import game_events_occurred as game_events_occurred_foml
-from ..qtable_agent.train import reward_from_events as reward_from_events_foml
-from ..qtable_agent.train import setup_training as setup_training_foml
+from ..qtable_agent.train import end_of_round as end_of_round_other
+from ..qtable_agent.train import game_events_occurred as game_events_occurred_other
+from ..qtable_agent.train import reward_from_events as reward_from_events_other
+from ..qtable_agent.train import setup_training as setup_training_other
 
+# Store the agents round rewards for comparison between rounds
 TOTAL_ROUND_REWARD = 0
 BEST_ROUND_REWARD = -10e10
 
 
 def setup_training(self):
-    setup_training_foml(self)
+    # Setup the other for training.
+    setup_training_other(self)
 
 
 def game_events_occurred(self, old_game_state, old_action, new_game_state, events):
-    game_events_occurred_foml(self, old_game_state, old_action, new_game_state, events)
+    # Let the other agent know that a game event has occurred.
+    game_events_occurred_other(self, old_game_state, old_action, new_game_state, events)
+
+    # Calculate the rewards for the step and add it to the total reward for this round.
     reward_from_events(self, events)
 
 
 def end_of_round(self, last_game_state, last_action, events):
     global TOTAL_ROUND_REWARD
     global BEST_ROUND_REWARD
-    end_of_round_foml(self, last_game_state, last_action, events)
+
+    # Let the other agent know that the round has ended.
+    end_of_round_other(self, last_game_state, last_action, events)
+    # Update the round reward.
     reward_from_events(self, events)
 
+    # Check if the round reward is the best reward so far.
     if TOTAL_ROUND_REWARD > BEST_ROUND_REWARD:
         print(f"Found Better Model: {TOTAL_ROUND_REWARD} > {BEST_ROUND_REWARD}")
         BEST_ROUND_REWARD = TOTAL_ROUND_REWARD
@@ -31,23 +40,17 @@ def end_of_round(self, last_game_state, last_action, events):
         # Store the model
         with open("my-saved-model.pt", "wb") as file:
             pickle.dump(self.model, file)
+    # Rollback to the best model every 100 rounds.
     elif last_game_state["round"] % 100 == 0:
         with open("my-saved-model.pt", "rb") as file:
             self.model = pickle.load(file)
 
-    # reset the TOTAL_ROUND_REWARD to 0
+    # Reset the TOTAL_ROUND_REWARD to 0.
     TOTAL_ROUND_REWARD = 0
 
 
 def reward_from_events(self, events):
-    """Sum all rewards from each step, reset at each end round.
-
-    Parameters
-    ----------
-    events : List
-        List of all events that occurred during the step.
-    """
-
     global TOTAL_ROUND_REWARD
-    reward_for_step = reward_from_events_foml(self, events)
+    # Calculate the rewards for the step and add it to the total reward for this round.
+    reward_for_step = reward_from_events_other(self, events)
     TOTAL_ROUND_REWARD += reward_for_step
